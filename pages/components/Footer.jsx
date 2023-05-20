07b3bcf644390d85b946fff7be722b9c2cfa41c2
0xgoldenlion <50057882+malawadd@users.noreply.github.com>
2023-05-20 15:17:48 +0300

    🍇 init web3 kit

---
+++ pages/components/Footer.jsx
@@ -0,0 +1,17 @@
+import { Container, Divider, Link, Text } from "@chakra-ui/react";
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <Divider w={"80%"} mx={"auto"} />
+      <Container py={"2rem"}>
+        <Text textAlign={"center"} fontSize={"1rem"}>
+         
+        </Text>
+      </Container>
+    </>
+  );
+}
+
+export default Footer;
